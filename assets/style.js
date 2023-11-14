09101e3d5c4c23397b2de3cfd1352b477ade1492
Michael Carmelo <michaelcarmelo22@gmail.com>
2023-11-14 00:38:31 -0700

    debugged the choicebuttons declaration

--- assets/style.js
+++ assets/style.js
@@ -23,74 +23,74 @@ const submitButton = document.getElementById("submit-score");
 let currentQuestionIndex = 0;
 let score = 0;
 let timer;
-let timerDisplay;
+let timerDisplay = document.getElementById("time-remaining");
 let timeLeft;
 const timerDuration = 60; // Set the timer duration in seconds
+let choiceButtons = [];
 
 // start quiz
 function startQuiz() {
-// start the timer
-startTimer();
+  // start the timer
+  startTimer();
 
-timerDisplay = document.getElementById("time-remaining");
-timeLeft = timerDuration;
+  timerDisplay = document.getElementById("time-remaining");
+  timeLeft = timerDuration;
 
+  timer = setInterval(function () {
+    if (timeLeft <= 0) {
+      clearInterval(timer); // Stop the timer
+      endGame();
+      gameOver();
+    } else {
+      timerDisplay.textContent = timeLeft; // Update the timer display
+      timeLeft--;
+    }
+  }, 1000); // The timer should run every 1 second (1000 milliseconds)
 
-timer = setInterval(function() {
-  if (timeLeft <= 0) {
-    clearInterval(timer); // Stop the timer
-    endGame();
-    gameOver();
-  } else {
-    timerDisplay.textContent = timeLeft; // Update the timer display
-    timeLeft--;
-  }
-}, 1000); // The timer should run every 1 second (1000 milliseconds)
+  // Show the question container
+  const questionContainer = document.getElementById("question-container");
+  questionContainer.classList.remove("hide");
 
-// Show the question container
-const questionContainer = document.getElementById("question-container");
-questionContainer.classList.remove("hide");
-
-// Display the first question
-displayQuestion(currentQuestionIndex);
+  // Display the first question
+  displayQuestion(currentQuestionIndex);
 }
-
-
 
 // Function to display a question and answer choices
 function displayQuestion(questionIndex) {
-const questionText = document.getElementById("question");
-choicesList = document.getElementById("choices");
+  const questionText = document.getElementById("question");
+  const choicesList = document.getElementById("choices");
 
-// Check if all questions have been answered
-if (questionIndex >= quizQuestions.length) {
-  endGame();
-  // Show the submit button when all questions have been answered
-  submitButton.style.display = "block";
-  return;
-} else {
-  // Hide the submit button for regular questions
-  submitButton.style.display = "none";
-}
+  // Check if all questions have been answered
+  if (questionIndex >= quizQuestions.length) {
+    endGame();
+    // Show the submit button when all questions have been answered
+    submitButton.style.display = "block";
+    return;
+  } else {
+    // Hide the submit button for regular questions
+    submitButton.style.display = "none";
+  }
 
-// Display the question and answer choices
-const question = quizQuestions[questionIndex];
-questionText.textContent = question.question;
-choicesList.innerHTML = "";
+  // Display the question and answer choices
+  const question = quizQuestions[questionIndex];
+  questionText.textContent = question.question;
+  choicesList.innerHTML = "";
 
-const choiceButtons = [];
+  // Create and append choice buttons
+  choiceButtons = [];
 
-question.choices.forEach((choice, index) => {
-  const choiceButton = document.createElement("button");
-  choiceButton.textContent = choice;
-  choiceButton.classList.add("choice");
-  choiceButton.addEventListener("click", () => handleAnswerClick(choice, question.correctAnswer, index));
-  choicesList.appendChild(choiceButton);
-});
+  question.choices.forEach((choice, index) => {
+    const choiceButton = document.createElement("button");
+    choiceButton.textContent = choice; // Set text content here
+    choiceButton.classList.add("choice");
+    choiceButton.addEventListener("click", () => handleAnswerClick(choice, question.correctAnswer, index));
+    choicesList.appendChild(choiceButton);
+    choiceButtons.push(choiceButton);
+  });
 
   choiceButtons.forEach((choiceButton) => {
-  choiceButton.removeEventListener("click", () => handleAnswerClick(choiceButton.textContent, question.correctAnswer, index));  
- });
+    choiceButton.removeEventListener("click", () => handleAnswerClick(choiceButton.textContent, quizQuestions[currentQuestionIndex].correctAnswer, index));
+  });
 }
 
 // Function to handle user clicks on answer choices
@@ -99,43 +99,52 @@ function handleAnswerClick(selectedAnswer, correctAnswer, choiceIndex) {
   const timerDisplay = document.getElementById("timer-remaining");
   const messageElement = document.getElementById("incorrect-message");
 
-if (selectedAnswer === correctAnswer) {
-  score++;
-  currentQuestionIndex++;
-} else {
-messageElement.textContent = `Incorrect! Select Again! -${timePenalty} penalty`;
-choiceButtons[choiceIndex].classList.add("incorrect");
-timeLeft -= timePenalty;//Taking away time because an answer was incorrect
+  if (selectedAnswer === correctAnswer) {
+    score++;
+  } else {
+    messageElement.textContent = `Incorrect! Select Again! -${timePenalty} penalty`;
+    choiceButtons[choiceIndex].classList.add("incorrect");
+    timeLeft -= timePenalty;
+  }
+
+  choiceButtons.forEach((button, index) => {
+    if (index === quizQuestions[currentQuestionIndex].choices.indexOf(correctAnswer)) {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+
+  setTimeout(function () {
+    currentQuestionIndex++;
+    displayQuestion(currentQuestionIndex);
+  }, 1000);
 }
 
-choiceButtons.forEach((button, index) => {
-  if (index === quizQuestions[questionIndex].choices.indexOf(correctAnswer)) {
-    button.classList.add("correct");
-  }
-});
-
-timerDisplay.textContent = timeLeft;
 
 displayQuestion(currentQuestionIndex);
-}
 
+
+
+// Function to start the timer
 function startTimer() {
-timer = setInterval(function () {
-  if (timeLeft <= 0) {
-    endGame();
-    clearInterval(timer);
-    gameOver();
-  } else {
-    timerDisplay.textContent = timeLeft;
-    timeLeft--;
-  }
-}, 1000); 
+  timer = setInterval(function () {
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+      endGame();
+      gameOver();
+    } else {
+      if (timerDisplay) {
+        // Update the timer display only if the element exists
+        timerDisplay.textContent = timeLeft;
+      }
+      timeLeft--;
+    }
+  }, 1000);
 }
-
 
 function endGame() {
-const finalScore = score;
-localStorage.setItem('highScore', finalScore);
+  const finalScore = score;
+  localStorage.setItem('highScore', finalScore);
 }
 
 const startButton = document.getElementById("start-button");
@@ -156,7 +165,7 @@ function gameOver() {
 
   questionContainer.classList.add("hide");
   choicesList.classList.add("hide");
-  submitButton.style.display="none";
+  submitButton.style.display = "none";
 
   const timerDisplay = document.getElementById("time-remaining");
   timerDisplay.textContent = "0";
@@ -166,9 +175,9 @@ function gameOver() {
 }
 
 const tryAgainButton = document.getElementById("try-again");
-tryAgainButton.addEventListener("click", () =>{
+tryAgainButton.addEventListener("click", () => {
   gameOverText.classList.add("hide");
   tryAgainButton.classList.add("hide");
 
-startQuiz();
+  startQuiz();
 });
